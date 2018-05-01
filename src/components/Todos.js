e9f1d11b078ad5b4dbeadb59e2740f7e5dbefed2
kingrayhan <rayhan095@gmail.com>
2018-05-01 10:24:41 +0600

    call props functions directly from Todos component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,35 +1,22 @@
 import React , { Component } from 'react'
 
 class Todos extends Component{
-
-
-	delete = todo => {
-		this.props.delete(todo);
-	}
-
-	toggleDone = todo =>
-	{
-		this.props.toggleDone(todo);
-	}
-	ALLLLLDone = () => {
-		this.props.allDone();
-	}
 	render()
 	{
 		return (
 			<React.Fragment>
 				<section className="main">
 					<label htmlFor="toggle-all">Mark all as complete</label>
-						<input id="toggle-all" className="toggle-all" type="checkbox" onClick={this.ALLLLLDone}/>
+						<input id="toggle-all" className="toggle-all" type="checkbox" onClick={() => { this.props.allDone() }}/>
 						<ul className="todo-list">
 							{this.props.todos.map( (todo , i) => (
 								<li key={i}
 									className={todo.done ? 'completed' : ''}
 								>
 									<div className="view">
-										<input className="toggle" type="checkbox" onChange={ () => { this.toggleDone(todo) } } checked={todo.done}/>
+										<input className="toggle" type="checkbox" onChange={ () => { this.props.toggleDone(todo) } } checked={todo.done}/>
 										<label>{todo.task}</label>
-										<button className="destroy" onClick={ () => { this.delete(todo) } }></button>
+										<button className="destroy" onClick={ () => { this.props.delete(todo) } }></button>
 									</div>
 									<input className="edit" />
 								</li>
